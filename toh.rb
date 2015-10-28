b068925265acb25d43e5bc3c8679756fc9fd10f4
Jeffrey Gisin <jgisin@gmail.com>
2015-10-28 10:06:28 -0600

    Bug fixing

--- toh.rb
+++ toh.rb
@@ -9,6 +9,7 @@ class Toh
 			end
 			puts ''
 		end
+		board
 	end
 
 	def get_position(selection)
@@ -20,8 +21,7 @@ class Toh
 			when 3
 				return :three
 	end
-			
-	end
+end
 
 	def move_piece(piece, position, board = {one: [1,2,3,4], two: [], three: []})
 		if board[:one].include?(piece)
@@ -37,31 +37,37 @@ class Toh
 		board
 	end
 
+	def logic(board)
+		puts "Please select which piece to move"
+		piece = gets.chomp.to_i
+			if piece < 5
+				puts "Please select where to move the piece"
+				selection = gets.chomp.to_i
+				if selection > 3 || selection < 1
+					puts "***** Please select again. 
+					The position must be from 1-3 *****"
+					render
+				else
+					if (board[get_position(selection)].sort.first == nil || 
+						piece < board[get_position(selection)].sort.first )
+						render(move_piece(piece, get_position(selection), board))
+					else
+						puts "***** Cant move big piece on smaller piece *****"
+					end
+				end
+			else
+				puts "***** Please select again. The piece must be from 1-4 *****"
+			end
+	end
+
+
 	def game
 		board = {one: [1,2,3,4], two: [], three: []}
 		render
 		while board[:three].length != 4
-			puts "Please select which piece to move"
-			piece = gets.chomp.to_i
-				if piece < 5
-					puts "Please select where to move the piece"
-					selection = gets.chomp.to_i
-					if selection > 3 || selection < 1
-						puts "***** Please select again. The position must be from 1-3 *****"
-						render
-					else
-						if board[get_position(selection)].sort.first == nil || piece < board[get_position(selection)].sort.first 
-							render(move_piece(piece, get_position(selection), board))
-						else
-							puts "***** Cant move big piece on smaller piece *****"
-						end
-					end
-				else
-					puts "***** Please select again. The piece must be from 1-4 *****"
-					render
-				end
+			logic(board)
 		end
-		puts "Congrats you win!!"
+		puts "Congrats you win!!"	
 	end
 
 end
